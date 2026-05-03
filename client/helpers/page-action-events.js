3b329db4705636ba3897c0c3619d0c7d2a80d3d1
PhilosophiMoonbeam <94211695+PhilosophiMoonbeam@users.noreply.github.com>
2026-05-03 00:30:47 -0400

    [verified] chore: decouple page action bus from Vue

--- client/helpers/page-action-events.js
+++ client/helpers/page-action-events.js
@@ -1,4 +1,4 @@
-const Vue = require('vue')
+const { createEventBus } = require('./simple-event-bus')
 
 const PAGE_EDIT_EVENT = 'pageEdit'
 const PAGE_HISTORY_EVENT = 'pageHistory'
@@ -7,97 +7,97 @@ const PAGE_CONVERT_EVENT = 'pageConvert'
 const PAGE_DUPLICATE_EVENT = 'pageDuplicate'
 const PAGE_MOVE_EVENT = 'pageMove'
 const PAGE_DELETE_EVENT = 'pageDelete'
-const pageActionBus = new Vue()
+const pageActionBus = createEventBus()
 
 function emitPageEdit () {
-  pageActionBus.$emit(PAGE_EDIT_EVENT)
+  pageActionBus.emit(PAGE_EDIT_EVENT)
 }
 
 function emitPageHistory () {
-  pageActionBus.$emit(PAGE_HISTORY_EVENT)
+  pageActionBus.emit(PAGE_HISTORY_EVENT)
 }
 
 function emitPageSource () {
-  pageActionBus.$emit(PAGE_SOURCE_EVENT)
+  pageActionBus.emit(PAGE_SOURCE_EVENT)
 }
 
 function emitPageConvert () {
-  pageActionBus.$emit(PAGE_CONVERT_EVENT)
+  pageActionBus.emit(PAGE_CONVERT_EVENT)
 }
 
 function emitPageDuplicate () {
-  pageActionBus.$emit(PAGE_DUPLICATE_EVENT)
+  pageActionBus.emit(PAGE_DUPLICATE_EVENT)
 }
 
 function emitPageMove () {
-  pageActionBus.$emit(PAGE_MOVE_EVENT)
+  pageActionBus.emit(PAGE_MOVE_EVENT)
 }
 
 function emitPageDelete () {
-  pageActionBus.$emit(PAGE_DELETE_EVENT)
+  pageActionBus.emit(PAGE_DELETE_EVENT)
 }
 
 function onPageEdit (handler) {
-  pageActionBus.$on(PAGE_EDIT_EVENT, handler)
+  pageActionBus.on(PAGE_EDIT_EVENT, handler)
 }
 
 function onPageHistory (handler) {
-  pageActionBus.$on(PAGE_HISTORY_EVENT, handler)
+  pageActionBus.on(PAGE_HISTORY_EVENT, handler)
 }
 
 function onPageSource (handler) {
-  pageActionBus.$on(PAGE_SOURCE_EVENT, handler)
+  pageActionBus.on(PAGE_SOURCE_EVENT, handler)
 }
 
 function onPageConvert (handler) {
-  pageActionBus.$on(PAGE_CONVERT_EVENT, handler)
+  pageActionBus.on(PAGE_CONVERT_EVENT, handler)
 }
 
 function onPageDuplicate (handler) {
-  pageActionBus.$on(PAGE_DUPLICATE_EVENT, handler)
+  pageActionBus.on(PAGE_DUPLICATE_EVENT, handler)
 }
 
 function onPageMove (handler) {
-  pageActionBus.$on(PAGE_MOVE_EVENT, handler)
+  pageActionBus.on(PAGE_MOVE_EVENT, handler)
 }
 
 function onPageDelete (handler) {
-  pageActionBus.$on(PAGE_DELETE_EVENT, handler)
+  pageActionBus.on(PAGE_DELETE_EVENT, handler)
 }
 
 function offPageEdit (handler) {
   if (!handler) { return }
-  pageActionBus.$off(PAGE_EDIT_EVENT, handler)
+  pageActionBus.off(PAGE_EDIT_EVENT, handler)
 }
 
 function offPageHistory (handler) {
   if (!handler) { return }
-  pageActionBus.$off(PAGE_HISTORY_EVENT, handler)
+  pageActionBus.off(PAGE_HISTORY_EVENT, handler)
 }
 
 function offPageSource (handler) {
   if (!handler) { return }
-  pageActionBus.$off(PAGE_SOURCE_EVENT, handler)
+  pageActionBus.off(PAGE_SOURCE_EVENT, handler)
 }
 
 function offPageConvert (handler) {
   if (!handler) { return }
-  pageActionBus.$off(PAGE_CONVERT_EVENT, handler)
+  pageActionBus.off(PAGE_CONVERT_EVENT, handler)
 }
 
 function offPageDuplicate (handler) {
   if (!handler) { return }
-  pageActionBus.$off(PAGE_DUPLICATE_EVENT, handler)
+  pageActionBus.off(PAGE_DUPLICATE_EVENT, handler)
 }
 
 function offPageMove (handler) {
   if (!handler) { return }
-  pageActionBus.$off(PAGE_MOVE_EVENT, handler)
+  pageActionBus.off(PAGE_MOVE_EVENT, handler)
 }
 
 function offPageDelete (handler) {
   if (!handler) { return }
-  pageActionBus.$off(PAGE_DELETE_EVENT, handler)
+  pageActionBus.off(PAGE_DELETE_EVENT, handler)
 }
 
 module.exports = {
--- client/helpers/page-action-events.test.js
+++ client/helpers/page-action-events.test.js
@@ -110,8 +110,10 @@ describe('page action event usage', () => {
   test('page action helper owns its bus instead of requiring caller root instances', () => {
     const source = fs.readFileSync(helperPath, 'utf8')
 
-    expect(source).toMatch(/const\s+Vue\s*=\s*require\(\s*['"]vue['"]\s*\)/)
-    expect(source).toMatch(/new\s+Vue\s*\(\s*\)/)
+    expect(source).toContain("require('./simple-event-bus')")
+    expect(source).not.toMatch(/require\(\s*['"]vue['"]\s*\)/)
+    expect(source).not.toMatch(/new\s+Vue\s*\(/)
+    expect(source).not.toMatch(/\.\$(?:emit|on|off)\s*\(/)
     expect(source).not.toMatch(/function\s+\w+\s*\(\s*root\b/)
     expect(source).not.toMatch(/\broot\s*\.\s*\$(?:emit|on|off)\b/)
   })
